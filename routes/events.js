69ed246320cf90cef71e68342a3604d65da3bc9a
= <=>
2013-01-19 01:02:27 +0100

    Write event post data to MongoDB

--- routes/events.js
+++ routes/events.js
@@ -6,3 +6,10 @@
 exports.addevent = function(req, res){
   res.render('addevent', { title: 'Express' });
 };
+
+exports.addeventpost = function(req, res){
+  	var databaseUrl = "devnology"; // "username:password@example.com/mydb"
+	var collections = ["events"];
+	var db = require("mongojs").connect(databaseUrl, collections);
+	db.events.save({title: req.body.event.title, description: req.body.event.description});
+}
