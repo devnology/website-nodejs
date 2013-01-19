10811abee14382ea4262a143f6144f949822aa0f
Pieter Joost van de Sande <pj@born2code.net>
2013-01-19 01:02:27 +0100

    Write event post data to MongoDB

--- app.js
+++ app.js
@@ -32,9 +32,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 
 app.get('/events/add', events.addevent);
-app.post('/events/add', function(req, res) {
-  console.log("whooaaa: " + req)  
-});
+app.post('/events/add', events.addeventpost);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
