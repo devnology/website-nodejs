f403efd0489994a25557cb6cb4e8e3ff6bf313d9
Pieter Joost van de Sande <pj@born2code.net>
2013-01-19 14:16:54 +0100

    Use Mongoose models

--- controllers/event/index.js
+++ controllers/event/index.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
-
 exports.engine = 'jade';
+
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectID;
+
+var EventSchema = new Schema({
+    title            : { type: String, required: true, trim: true },
+    content            : { type: String, required: true, trim: true }
+});
+
+var Event = mongoose.model('Event', EventSchema);
 
 exports.edit = function(req, res, next){
   var body = req.body;
@@ -9,7 +18,8 @@ exports.edit = function(req, res, next){
 };
 
 exports.add = function(req, res, next){
-  res.render('add', {});
+	var newEvent = new Event();
+  res.render('add', {event : newEvent});
 }
 
 exports.create = function(req, res, next){
