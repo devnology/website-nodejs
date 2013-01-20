35a0247507ba00ab161c351522007d201d170f2a
Pieter Joost van de Sande <pj@born2code.net>
2013-01-20 01:32:01 +0100

    Improve event editing

--- controllers/event/index.js
+++ controllers/event/index.js
@@ -2,12 +2,6 @@ var mongoose = require('mongoose'),
 	Event = require('../../models/event.js');
 exports.engine = 'jade';
 
-exports.edit = function(req, res, next) {
-	var body = req.body;
-	res.message('Information updated!');
-	res.redirect('/event/' + req.pet.id);
-}
-
 exports.add = function(req, res, next) {
 	var newEvent = new Event({title: 'arne', content: 'inserten'});
   	res.render('add', {event: newEvent});
@@ -18,6 +12,52 @@ exports.create = function(req, res, next){
 
 	var newEvent = new Event(req.body.event);
 
+	newEvent.save(function(error){
+	    if(error){
+	        res.json(error);
+	    }
+	    else{
+	        res.redirect('/events')
+	    }
+	});
+}
+
+exports.edit = function(req, res, next){
+	var id = req.params.event_id;
+	Event.findById(id, function(err, e) {
+		if(err) return next(err);
+
+		res.render('edit', {event: e});
+	});
+}
+
+exports.show = function(req, res, next){
+	var id = req.params.event_id;
+	if(!id) return next(new Error("missing event_id parameter"));
+
+	Event.findById(id, function(err, e) {
+		if(err) return next(err);
+
+		res.render('edit', {event: e});
+	});	
+}
+
+exports.update = function(req, res, next){
+	var id = req.params.event_id;
+	var e = req.body.event;
+	if(!id) return next(new Error("missing event_id parameter"));
+	if(!e) return next(new Error("missing event post data"));
+
+	Event.update({_id:id}, {title: e.title, content: e.content, start: e.start, end: e.end}, function (err) {
+    	if (err) return next(err);
+  		
+  		res.redirect("/event/"+id)
+	});
+}
+
+exports.create = function(req, res, next){
+
+	var newEvent = new Event(req.body.event);
 	newEvent.save(function(error){
 	    if(error){
 	        res.json(error);
--- models/event.js
+++ models/event.js
@@ -2,8 +2,11 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var EventSchema = new Schema({
-	title : { type: String, required: true, trim: true },
-	content : { type: String, required: true, trim: true }
+	title     : { type: String, required: true, trim: true },
+	content   : { type: String, required: true, trim: true },
+	start     : {type: Date },
+	end       : {type: Date },
+	published : {type: Boolean }
 });
 
 module.exports = mongoose.model('Event', EventSchema);
